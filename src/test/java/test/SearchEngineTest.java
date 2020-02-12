b39255a23065bf1f4806bacda57f95e53106c3d9
Marcus <mannils233@gmail.com>
2020-02-12 21:10:42 +0100

    Init project

---
+++ src/test/java/test/SearchEngineTest.java
@@ -0,0 +1,12 @@
+package test;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SearchEngineTest {
+
+    @Test
+    public void test() {
+    }
+}
